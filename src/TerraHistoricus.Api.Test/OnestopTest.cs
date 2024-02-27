0eba7628d9146fec84cbefce49cce4c0383a2505
Baka632 <stevemc123456@outlook.com>
2024-02-27 16:49:00 +0800

    移除不应当添加的代码

--- src/TerraHistoricus.Api.Test/OnestopTest.cs
+++ src/TerraHistoricus.Api.Test/OnestopTest.cs
@@ -10,8 +10,6 @@ public class OneStopTest
     // [Fact]
     public async void OneStop()
     {
-        using StreamWriter writer = File.CreateText(@"E:\Temp\泰拉记事社.txt");
-
         PageInfo defaultPageInfo = default;
         PageDetail defaultPageDetail = default;
         IEnumerable<ComicInfo> allComics = await ComicService.GetAllComicAsync();
@@ -19,24 +17,19 @@ public class OneStopTest
         foreach (ComicInfo comicInfo in allComics)
         {
             ComicDetail comicDetail = await ComicService.GetComicDetailAsync(comicInfo.Cid);
-            writer.WriteLine(comicDetail);
 
             foreach (EpisodeInfo episodeInfo in comicDetail.Episodes)
             {
                 EpisodeDetail episodeDetail = await EpisodeAndPageService.GetEpisodeDetailAsync(comicDetail.Cid, episodeInfo.Cid);
-                writer.WriteLine($"    {episodeDetail}");
 
                 for (int i = 0; i < episodeDetail.PageInfos.Count; i++)
                 {
                     PageInfo pageInfo = episodeDetail.PageInfos[i];
                     Assert.NotEqual(defaultPageInfo, pageInfo);
-                    writer.WriteLine($"        {pageInfo}");
 
                     int pageNumber = i + 1;
                     PageDetail pageDetail = await EpisodeAndPageService.GetPageDetailAsync(comicDetail.Cid, episodeInfo.Cid, pageNumber);
                     Assert.NotEqual(defaultPageDetail, pageDetail);
-
-                    writer.WriteLine($"        {pageDetail}");
                 }
             }
         }
